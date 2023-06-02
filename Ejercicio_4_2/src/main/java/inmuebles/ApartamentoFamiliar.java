dfd842054e3aab260fa5d83548c2002b0206d64d
Jteheranm <jteheranm@unal.edu.co>
2023-06-02 10:27:34 -0500

    Agregado el atributo de pisos a la clase ApartamentoFamiliar

--- Ejercicio_4_2/src/main/java/inmuebles/ApartamentoFamiliar.java
+++ Ejercicio_4_2/src/main/java/inmuebles/ApartamentoFamiliar.java
@@ -4,10 +4,13 @@ public class ApartamentoFamiliar extends Apartamento {
     protected static double valorArea = 2000000;
     // Atributo para asignar el valor de administracion
     protected int valorAdm;
+    // Numero de pisos del apt
+    protected int pisos;
 
     //Constructor
-    public ApartamentoFamiliar(int idInmueble, String direccion, int area, int numHabitaciones, int numBanos, int piso, int valorAdm){
+    public ApartamentoFamiliar(int idInmueble, String direccion, int area, int numHabitaciones, int numBanos, int pisos, int valorAdm){
         super(idInmueble, direccion, area, numHabitaciones, numBanos);
+        this.pisos = pisos;
         this.valorAdm = valorAdm;
     }
 
