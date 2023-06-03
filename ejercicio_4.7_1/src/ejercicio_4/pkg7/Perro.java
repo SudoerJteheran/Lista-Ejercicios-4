31d45fd3546191edb579e43ac6c1ebe3e32384f3
Juan Manuel Teherán Machado <79525668+SudoerJteheran@users.noreply.github.com>
2023-06-03 12:39:25 -0500

    Update Perro.java
    Identaito

--- ejercicio_4.7_1/src/ejercicio_4/pkg7/Perro.java
+++ ejercicio_4.7_1/src/ejercicio_4/pkg7/Perro.java
@@ -10,32 +10,32 @@ package ejercicio_4.pkg7;
  */
 public class Perro extends Cánidos {
     @Override
-public String getSonido() {
- return "Ladrido";
+    public String getSonido() {
+        return "Ladrido";
     }
 /**
 * Método que devuelve un String con los alimentos de un perro
 * @return Un valor String con la alimentación de un perro: “Carnívoro”
 */
 @Override
-public String getAlimentos() {
-return "Carnivoro";
-}
+    public String getAlimentos() {
+        return "Carnivoro";
+    }
 /**
 * Método que devuelve un String con el hábitat de un perro
 * @return Un valor String con el hábitat de un perro: “Doméstico”
 */
 @Override
-public String getHábitat() {
-return "Domestico";
-}
+    public String getHábitat() {
+        return "Domestico";
+    }
 /**
 * Método que devuelve un String con el nombre científico de un perro
 * @return Un valor String con el nombre científico de un perro:
 * “Canis lupus familiaris”
 */
 @Override
-public String getNombreCientífico() {
-return "Canis lupus familiaris";
-}
+    public String getNombreCientífico() {
+        return "Canis lupus familiaris";
+    }
 }
